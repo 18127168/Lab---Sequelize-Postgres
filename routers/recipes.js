6bafdc9d341c664ac2748dcc04e3b2535089d5a2
18127168 <50893659+18127168@users.noreply.github.com>
2020-12-12 01:44:58 +0700

    Update recipes.js

--- routers/recipes.js
+++ routers/recipes.js
@@ -9,7 +9,8 @@ router.get('/', (req, res) => {
     controller.getRecipes(function(recipes) {
         controller.getIngredient(function(ingredients) {
             recipes = recipes.slice(0, 5);
-            ingredients = ingredients.slice(0, 20);
+            ingredients = ingredients.slice(0, 5) + ingredients.slice(10, 15) + ingredients.slice(20, 25) + ingredients(30, 35);
+            
             res.render('recipes',{recipes :recipes, ingredients: ingredients});
         });
     });
