518c706945f2699d93980487d9c0a9f9564f0a9a
18127168 <tranbui.tainhan1002@gmail.com>
2020-12-15 16:43:19 +0700

    fix search

--- routers/search.js
+++ routers/search.js
@@ -8,11 +8,19 @@ router.get('/', (req, res) => {
     var query = req.query.query;
     controller.search(query, function(search_recipes) {
         controller.getIngredient(function(ingredients) {
-            console.log(search_recipes);
+            search_recipes = search_recipes.slice(0, search_recipes.length/2);
+
             for (let i = 0; i < search_recipes.length; i++){
                 search_recipes[i].id = i + 1;
             }
-            console.log(search_recipes);
+
+            for (let i = 0; i < 5; i++){
+                ingredients[i*4].RecipeId = i + 1;
+                ingredients[i*4 + 1].RecipeId = i + 1;
+                ingredients[i*4 + 2].RecipeId = i + 1;
+                ingredients[i*4 + 3].RecipeId = i + 1;
+            }
+
             res.render('recipes',{recipes :search_recipes, ingredients: ingredients});
         });
     });
